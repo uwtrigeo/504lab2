ce3d5dae1d554c469a6fd812bd5293451dcf7422
uwtrigeo <115599976+uwtrigeo@users.noreply.github.com>
2023-01-11 14:16:14 -0800

    update

---
+++ data/hospitals.js
@@ -0,0 +1,13 @@
+var hospitalPoints = {
+    "type":"FeatureCollection",
+    "features":[
+      {"type":"Feature","geometry":{"type":"Point","coordinates":[-122.48083069805091,47.24158294247902]},"properties":{"NAME":"Allenmore Hospital","ADDRESS":"1901 S UNION AVE","CITY":"Tacoma","ZIP":98405}},
+      {"type":"Feature","geometry":{"type":"Point","coordinates":[-122.29012358186617,47.17861596384059]},"properties":{"NAME":"Good Samaritan Hospital","ADDRESS":"401 15TH AVE SE","CITY":"Puyallup","ZIP":98372}},
+      {"type":"Feature","geometry":{"type":"Point","coordinates":[-122.55190153531765,47.109023185217126]},"properties":{"NAME":"Madigan Hospital","ADDRESS":"9040 JACKSON AVE\r\n","CITY":"Tacoma","ZIP":98431}},
+      {"type":"Feature","geometry":{"type":"Point","coordinates":[-122.50212346172164,47.15457916145514]},"properties":{"NAME":"St Clare Hospital","ADDRESS":"11315 BRIDGEPORT WAY SW","CITY":"Lakewood","ZIP":98499}},
+      {"type":"Feature","geometry":{"type":"Point","coordinates":[-122.44793640184797,47.24530738056208]},"properties":{"NAME":"St Joseph Medical Center","ADDRESS":"1717 S J ST","CITY":"Tacoma","ZIP":98405}},
+      {"type":"Feature","geometry":{"type":"Point","coordinates":[-122.45305284917963,47.25946004827884]},"properties":{"NAME":"Tacoma General Hospital","ADDRESS":"315 MARTIN LUTHER KING JR WAY","CITY":"Tacoma","ZIP":98405}},
+      {"type":"Feature","geometry":{"type":"Point","coordinates":[-122.61346848763097,47.3644245711541]},"properties":{"NAME":"St Anthony Hospital","ADDRESS":"11567 CANTERWOOD BLVD NW","CITY":"Gig Harbor","ZIP":98332}},
+      {"type":"Feature","geometry":{"type":"Point","coordinates":[-122.57559702458708,47.13526338360076]},"properties":{"NAME":"VA Puget Sound Health - American Lake","ADDRESS":"9600 VETERANS DR SW","CITY":"Tacoma","ZIP":98493}}
+    ]
+  };
--- scripts.js
+++ scripts.js
@@ -5,3 +5,84 @@ const map = new mapboxgl.Map({
   center: [-122.4443, 47.2529], // starting position
   zoom: 10 // starting zoom
 });
+
+// Hospital & library point layers added to map
+map.on('load', function() {
+    map.addLayer({
+      id: 'hospitals',
+      type: 'symbol',
+      source: {
+        type: 'geojson',
+        data: hospitalPoints
+      },
+      layout: {
+        'icon-image': 'hospital-15',
+        'icon-allow-overlap': true
+      },
+      paint: { }
+    });
+    map.addLayer({
+      id: 'libraries',
+      type: 'symbol',
+      source: {
+        type: 'geojson',
+        data: libraryPoints
+      },
+      layout: {
+        'icon-image': 'library-15',
+        'icon-allow-overlap': true
+      },
+      paint: { }
+    });
+    
+    map.addSource('nearest-hospital', {
+        type: 'geojson',
+        data: {
+          type: 'FeatureCollection',
+          features: [
+          ]
+        }
+      });
+    
+  });
+
+//   hospital popup
+var popup = new mapboxgl.Popup();
+    map.on('click', 'hospitals', function(e) {
+    var feature = e.features[0];
+    popup.setLngLat(feature.geometry.coordinates)
+    .setHTML(feature.properties.NAME + '<br>' + feature.properties.ADDRESS + '<br>' + feature.properties.CITY + ', WA ' + feature.properties.ZIP)
+    .addTo(map);
+});
+
+// turf event listener
+map.on('click', 'libraries', function(f) {
+  // Using Turf, find the nearest hospital to library clicked
+  var refLibrary = f.features[0];
+  var nearestHospital = turf.nearest(refLibrary, hospitalPoints);
+
+  	// Update the 'nearest-hospital' data source to include the nearest library
+	map.getSource('nearest-hospital').setData({
+        type: 'FeatureCollection',
+        features: [
+         nearestHospital
+        ]
+  });
+
+// Create a new circle layer from the 'nearest-hospital' data source
+map.addLayer({
+        id: 'nearestHospitalLayer',
+  type: 'circle',
+  source: 'nearest-hospital',
+  paint: {
+  'circle-radius': 12,
+  'circle-color': '#486DE0'
+  }
+}, 'hospitals');
+
+//Add popup that gives name of the library and the name and address of the nearest hospital
+popup.setLngLat(refLibrary.geometry.coordinates)
+    .setHTML('<b>' + refLibrary.properties.BRANCH +  ' Branch Library' + '</b><br>The nearest hospital is ' + nearestHospital.properties.NAME + ', located at ' + nearestHospital.properties.ADDRESS)
+    .addTo(map);
+
+});
